ef77656edcdbb8704ddfcaf45116db060a2bf470
StarLordNumber1 <60734541+StarLordNumber1@users.noreply.github.com>
2021-04-21 09:43:12 +0200

    Door IT

---
+++ Microwave.Test.Integration/TD_IT1_Door_UI.cs
@@ -0,0 +1,41 @@
+﻿using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class TD_IT1_Door_UI
+    {
+        private ICookController _fakeCoockController;
+        private IDisplay _fakeDisplay;
+        private ILight _fakeLight;
+        private Door _sut;
+        private IUserInterface _userInterface; 
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeCoockController = Substitute.For<ICookController>();
+            _fakeDisplay = Substitute.For<IDisplay>();
+            _fakeLight = Substitute.For<ILight>();
+
+            _userInterface = new UserInterface();
+
+            _sut = new Door();
+
+        }
+
+
+        
+
+
+
+
+
+
+    }
+    
+}
--- Microwave.Test.Integration/TD_Test.cs
+++ Microwave.Test.Integration/TD_Test.cs
@@ -7,4 +7,6 @@ namespace Microwave.Test.Integration
    {
       
    }
+
+
 }
